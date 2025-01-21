c194d59012e747fd9fca526f06774adda77bc71d
HiraM2001 <142256095+HiraM2001@users.noreply.github.com>
2025-01-22 03:06:12 +0500

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,7 +1,5 @@
 async function main() {
     const MyNFT = await ethers.getContractFactory("MyNFT");
- 
-    // Start deployment, returning a promise that resolves to a contract object
     const myNFT = await MyNFT.deploy();
     console.log("Contract deployed to address:", myNFT.address);
   }
